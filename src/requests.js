7233196dcf0db16aaf476c591197364843c49a2e
NaelDarken <Bsheer.work@gmail.com>
2019-02-08 09:42:21 +0200

    Initial Commit

---
+++ src/requests.js
@@ -0,0 +1,17 @@
+
+export {
+
+    puzzling as default
+
+}
+
+const puzzling = async (wordCount) => {
+    const response = await fetch(`https://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+    
+    if (response.status === 200) {
+        const data = await response.json()
+        return data.puzzle
+    } else {
+        throw new Error('Unable to get puzzle')
+    }
+}
